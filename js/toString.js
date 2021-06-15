c81c9022f612ca6bc75f1fdc85ef46db397497c6
Марина <maryfadeeva1208@gmail.com>
2021-06-15 17:17:53 +0300

    toString

---
+++ js/toString.js
@@ -0,0 +1,118 @@
+// To string
+
+// Number to string
+let value = 0;
+
+// 1 способ
+
+let toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+// 2 способ
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+
+value = 1;
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+//Infinity
+value = Infinity;
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+
+//NaN
+value = NaN;
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+
+//--------Boolean------- 
+//true to string
+
+value = true;
+
+// 1 способ
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+// 2 способ
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+
+//false to string
+
+value = false;
+
+// 1 способ
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+// 2 способ
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+//undefined
+
+value = undefined;
+
+// 1 способ
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+// 2 способ
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+
+//null
+
+value = null;
+
+// 1 способ
+
+toString = String(value);
+console.log(`${value}, через конструктор String():`, toString);
+console.log(`тип данных ${value}, через конструктор String():`, typeof toString);
+
+// 2 способ
+
+toString = value + "";
+console.log(`${value}, через конкатенацию:`, toString);
+console.log(`тип данных ${value}, через конкатенацию:`, typeof toString);
+

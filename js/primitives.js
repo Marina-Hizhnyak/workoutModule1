02f5b813c23800c2e95e538359572eea7ec260a3
Марина <maryfadeeva1208@gmail.com>
2021-06-15 16:19:47 +0300

    variables+primitives

---
+++ js/primitives.js
@@ -0,0 +1,55 @@
+console.log("primitives");
+
+//String строка
+
+let stringData = "JavaScript";
+console.log(stringData);
+console.log(typeof stringData); 
+
+stringData = "";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = " ";
+console.log(stringData);
+console.log(typeof stringData);
+
+// Number
+
+let number = 5;
+console.log(number);
+console.log(typeof number);
+
+number = 2e53;
+console.log(number);
+console.log(typeof number);
+
+number = Infinity;
+console.log(number);
+console.log(typeof number);
+
+number = NaN;
+console.log(number);
+console.log(typeof number);
+
+// Boolean  true || false
+
+let isOnline = true;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+// Undefined
+
+let msg;
+console.log(msg);
+console.log(typeof msg);
+
+//Null
+
+let user = null;
+console.log(user);
+console.log(typeof user);   //  object!!!
---
+++ js/variables.js
@@ -0,0 +1,37 @@
+console.log(student); //  undefined
+
+var student = "Marina";
+console.log(student);
+
+var count;
+console.log(count); //  undefined
+
+count = 5;
+console.log(count);
+
+count = 10;
+console.log(count);
+
+// console.log(number); // Ошибка: Uncaught ReferenceError: Cannot access 'number' before initialization at 1-1.js:7
+    
+let number = 100;
+console.log(number);
+
+let age;
+console.log(age); //  undefined
+
+age = 20;
+console.log(age);
+
+age = 35;
+console.log(age);
+
+// console.log(userName); //Uncaught ReferenceError: Cannot access 'userName' before initialization at 1-1.js:12
+    
+const userName = "LoveJS";
+console.log(userName);
+
+
+// const User; //Uncaught SyntaxError: Missing initializer in const declaration
+
+// userName= Uncaught SyntaxError: Unexpected end of input

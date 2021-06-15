fab77dead485d5aa4983039b506ff805f09719d4
Марина <maryfadeeva1208@gmail.com>
2021-06-15 19:56:47 +0300

    toBoolean

---
+++ js/toBoolean.js
@@ -0,0 +1,149 @@
+// String to boolean
+let value = "Ya ya ya"
+
+//1 способ
+
+let toBoolean = Boolean(value);
+console.log(`${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// String number to boolean
+value = "12345"
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(`${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// Empty srting to boolean
+value = ""
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(`пустая строка ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных пустой строки ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`пустая строка ${value}, через !!`, toBoolean);
+console.log(`тип данных пустой строки ${value}, через !!`, typeof toBoolean);
+
+// Empty srting with space to boolean
+value = " "
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(`строка с пробелом ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных строки с пробелом${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`строка с пробелом ${value}, через !!`, toBoolean);
+console.log(`тип данных строки с пробелом ${value}, через !!`, typeof toBoolean);
+
+// Number to boolean
+value = 0
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// Number to boolean
+value = 1
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// Undefined to boolean
+value = undefined
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// Null to boolean
+value = null
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// NaN to boolean
+value = NaN
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
+
+// Infinity to boolean
+value = Infinity
+
+//1 способ
+
+toBoolean = Boolean(value);
+console.log(` ${value}, через конструктор Boolean():`, toBoolean);
+console.log(`тип данных ${value}, через конструктор Boolean():`, typeof toBoolean);
+
+// 2 способ
+
+toBoolean = !!value;
+console.log(`${value}, через !!`, toBoolean);
+console.log(`тип данных ${value}, через !!`, typeof toBoolean);
